0ff13402dace3249bd88248e395d39983859564e
bach2o <bachchakmak@gmail.com>
2018-01-29 02:09:19 +0700

    initial commit

---
+++ Session5/Homework/bacteria.py
@@ -0,0 +1,7 @@
+import math
+while True:
+    starting_bacteria = int(input('How many B bacterias are there? '))
+    time = int(input('How much time in minutes will we wait? '))
+    # result = starting_bacteria*2**math.floor(time/2)
+    result = starting_bacteria << (time // 2)
+    print('After', time, 'minute(s) we would have', result, 'B bacterias')
